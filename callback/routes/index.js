c826a68cf371fd29feabe8f6d8cd7ffcf392b522
Lakshmanan  J <lakshmanan_j@photonint.com>
2019-02-27 14:30:58 +0530

    program for merge array

--- callback/routes/index.js
+++ callback/routes/index.js
@@ -35,7 +35,6 @@ router.get('/callback', function(req,res,next){
       console.log("value "+element)
       callback(element);
     });
-   
   }
 
   function f2(value){
@@ -45,16 +44,68 @@ router.get('/callback', function(req,res,next){
       }
     })
     console.log(b+ "  value of  array "+JSON.stringify(finalValue))
-  }
-
-
-  
-  function display(){
-    console.log("print")
-  }
+  } 
   f1(a,f2);
   res.render('index', { JSONValue: JSON.stringify(finalValue) });
 })
+
+router.get('/mergeArray', function(req,res,next){
+
+    var a = [1,2,3,4,5];
+    var b = [3,4,5,6,7,8,9];
+    var mergeValue = [];
+    var temp = [];
+
+    b.forEach(value => {
+      a.push(value);
+    })
+
+    function removeDuplicate(){
+     
+      a.forEach(element => {
+        var booleanValue =isInArray(element);
+        if(booleanValue == false)
+        temp.push(element)
+      })
+    }
+    function isInArray(value) {
+      if(temp.length != 0)
+        return temp.indexOf(value) > -1;
+      else
+        return false
+    }
+    function reverseArray(){
+      let tempLength = temp.length - 1;
+      while(tempLength >= 0){
+        mergeValue.push(temp[tempLength]);
+        tempLength--;
+      }
+    }
+
+    removeDuplicate();
+    reverseArray();
+    console.log("merger Value "+temp);
+    console.log("reverse array "+mergeValue)
+    // console.log(b)
+
+    // while(bOfLength >= 0){
+    //   // console.log(a[bOfLength])
+    //   for(var i = 0; i<= b.length-1; i++){
+        
+    //     if(b[i] != a[bOfLength]){
+    //       console.log("b="+b[i]+" a="+a[bOfLength])
+    //       b.push(a[bOfLength])
+
+    //     }
+    //   }
+    //   bOfLength--;
+    // }
+console.log("value of b ="+b)
+
+
+
+
+});
 
 router.get('/promise', function(req,res,next){
   var currentStatus;
